082d0b6857a08f13f3320330347a5d4d840fe3e6
Kouhei Suzuki <kouhei.szk@gmail.com>
2016-09-17 21:40:33 +0900

    pyaudioを使って録音できるようにする

--- app.py
+++ app.py
@@ -1,8 +1,45 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+import pyaudio
+import wave
+
 
 def main():
-    print("hoge")
+    chunk = 1024
+    format = pyaudio.paInt16
+    channels = 2
+    rate = 44100
+    record_seconds = 5
+    wave_output_filename = "output.wav"
+
+    p = pyaudio.PyAudio()
+
+    stream = p.open(format=format,
+                    channels=channels,
+                    rate=rate,
+                    input=True,
+                    frames_per_buffer=chunk)
+
+    print("* recording")
+
+    frames = []
+
+    for i in range(0, int(rate / chunk * record_seconds)):
+        data = stream.read(chunk)
+        frames.append(data)
+
+    print("* done recording")
+
+    stream.stop_stream()
+    stream.close()
+    p.terminate()
+
+    wf = wave.open(wave_output_filename, 'wb')
+    wf.setnchannels(channels)
+    wf.setsampwidth(p.get_sample_size(format))
+    wf.setframerate(rate)
+    wf.writeframes(b''.join(frames))
+    wf.close()
 
 
 if __name__ == "__main__":
