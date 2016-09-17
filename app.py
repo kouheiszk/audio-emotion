6611ba156178bfe5a704509077894ec00c2ebaba
Kouhei Suzuki <kouhei.szk@gmail.com>
2016-09-18 02:10:42 +0900

    音声ファイルを分割して保存できるようにする

--- app.py
+++ app.py
@@ -1,46 +1,101 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 import atexit
+import logging
+import wave
 
 import numpy as np
 import pyaudio
 import time
 
+log = logging.getLogger("audio_emotion")
 
-class Spectrum(object):
+
+class Calibrater(object):
+    CALIBRATION_TIME = 5  # 5sec
+    THRESHOLD = 1.25
+
+    def __init__(self):
+        self.finished = False
+        self.base = None
+        self.averages = []
+
+    def calibrate(self, average, rate, buffer):
+        if not self.finished:
+            needs = self.CALIBRATION_TIME * rate / buffer
+
+            if len(self.averages) == 0:
+                log.debug("Start calibration...")
+
+            self.averages.append(average)
+
+            if len(self.averages) >= needs:
+                log.debug("Finish calibration.")
+                self.finished = True
+                self.base = sum(self.averages) / needs
+
+    def is_over_amplitude(self, average):
+        return average > self.base * self.THRESHOLD
+
+
+class Recorder(object):
+    SILENT_SPLIT_DURATION = 1  # 1sec
+
+    def __init__(self):
+        self.frames = []
+        self.start_time = None
+        self.finish_time = None
+
+    def record(self, samples, is_over_amplitude, channels, sampwidth, rate):
+        if is_over_amplitude:
+            if self.start_time is None:
+                log.debug("Start recording...")
+                self.start_time = time.time()
+                self.frames = []
+            self.finish_time = time.time() + 1.0
+
+        if self.start_time and self.finish_time:
+            self.frames.append(samples)
+
+        if self.finish_time and time.time() > self.finish_time:
+            filename = "{}.wav".format(int(self.start_time))
+            wf = wave.open(filename, "wb")
+            wf.setnchannels(channels)
+            wf.setsampwidth(sampwidth)
+            wf.setframerate(rate)
+            wf.writeframes(b"".join(self.frames))
+            wf.close()
+
+            log.debug("Finish recording, Write audio : {}".format(filename))
+
+            self.start_time = None
+            self.finish_time = None
+
+        pass
+
+
+class AudioManager(object):
     FORMAT = pyaudio.paFloat32
     CHANNELS = 1
-    RATE = 16000
+    RATE = 44100  # 44.1kHz
+    FRAMES_PER_BUFFER = 512
 
     def __init__(self):
         self.pa = pyaudio.PyAudio()
-        self.last_samples = None
+        self.calibrater = Calibrater()
+        self.recorder = Recorder()
+
         atexit.register(self.pa.terminate)
 
-    def fft(self, samples):
-        win = np.hanning(len(samples))
-        res = np.fft.fftshift(np.fft.fft(win * samples))
-        freq = np.fft.fftfreq(len(samples), d=self.RATE ** -1)
-        return list(zip(freq, 20 * np.log10(np.abs(res))))
-
-    def callback(self, in_data, frame_count, time_info, status):
-        data = np.fromstring(in_data, np.float32)
-        pr = []
-        for f, v in self.fft(data)[256 - 64:256]:
-            pr.append(str(min(9, max(0, int((v + 50) / 10)))))
-        print("".join(pr))
-        return (in_data, self.recording)
-
-    def record(self):
+    def start_recording(self):
         self.recording = pyaudio.paContinue
         stream = self.pa.open(format=self.FORMAT,
                               channels=self.CHANNELS,
                               rate=self.RATE,
                               input=True,
                               output=False,
-                              # frames_per_buffer = self.FRAME_LEN,
-                              frames_per_buffer=512,
-                              stream_callback=self.callback)
+                              frames_per_buffer=self.FRAMES_PER_BUFFER,
+                              stream_callback=self._callback)
         stream.start_stream()
 
         while stream.is_active():
@@ -49,13 +104,31 @@ class Spectrum(object):
             except KeyboardInterrupt:
                 self.recording = pyaudio.paAbort
 
-        stream.start_stream()
+        stream.stop_stream()
         stream.close()
+
+    def _callback(self, in_data, frame_count, time_info, status):
+        data = np.fromstring(in_data, np.float32)
+        average = np.average(np.abs(data))
+        if not self.calibrater.finished:
+            self.calibrater.calibrate(average, self.RATE, self.FRAMES_PER_BUFFER)
+        else:
+            is_over_amplitude = self.calibrater.is_over_amplitude(average)
+            self.recorder.record(in_data,
+                                 is_over_amplitude=is_over_amplitude,
+                                 channels=self.CHANNELS,
+                                 sampwidth=self.pa.get_sample_size(self.FORMAT),
+                                 rate=self.RATE)
+
+        return (in_data, self.recording)
 
 
 def main():
-    spe = Spectrum()
-    spe.record()
+    # ログフォーマットの設定
+    logging.basicConfig(level=logging.DEBUG, format='%(asctime)s [%(module)10s] [%(levelname)5s] %(message)s')
+
+    am = AudioManager()
+    am.start_recording()
 
 
 # def main():
